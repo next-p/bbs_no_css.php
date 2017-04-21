f38cc277faad099b1df67451204d59e9969c1db6
川口真那人 <nexseed0406@gmail.com>
2017-04-21 11:21:28 +0800

    変更

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -10,6 +10,10 @@
   $dbh = new PDO($dsn, $user, $password);
   $dbh->query('SET NAMES utf8');
 
+  //配列で取得したデータを格納
+   //配列を初期化 
+   $post_datas = array();
+
   //POST送信されたらINSERT文を実行
   if (!empty ($_POST)) {
     $nickname = $_POST['nickname'];
@@ -20,12 +24,52 @@
   $sql = 'INSERT INTO `posts` (`nickname`,`comment`,`created`)
   VALUES ("'.$nickname.'","'.$comment. '",now());';
 
+  // var_dump($sql);
+
+  //実行
   $stmt = $dbh->prepare($sql);
   $stmt->execute();
+
+  //select文の実行
+  //sql文作成(select文)
+  $sql = 'SELECT * FROM `posts`;';
+
+
+  
+
+   //実行
+   $stmt = $dbh->prepare($sql);
+   $stmt->execute();
+
+   //配列で取得したデータを格納
+   //配列を初期化
+   $post_datas = array();
+
+   //繰り返し文でデータの取得（フェッチ）
+   while (1) {
+     $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+     if ($rec ==false) {
+        break;
+
+      } 
+       //echo $rec{'nickname'};
+       $post_datas[] = $rec;
+   }
+
+
+  
+
+
+
   }
 
    // ３．データベースを切断する
    $dbh = null;
+
+ 
+ 
+
+
 	
 
 ?>
@@ -42,6 +86,14 @@
       <p><button type="submit" >つぶやく</button></p>
     </form>
     <!-- ここにニックネーム、つぶやいた内容、日付を表示する -->
+    <?php 
+        foreach ($post_datas as $post => $post_each) {
+          echo $post_each ['nickname'].'<br>';
+          echo $post_each ['comment'].'<br>';
+          echo $post_each ['created'].'<br>';
+          echo '<hr>';
+        }
+     ?>
 
 </body>
 </html>
